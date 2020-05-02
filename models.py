61f8de5931664acce81a63f70673cd611acd45d4
Lauri Eneh <enehlauri@gmail.com>
2020-05-02 15:38:03 +0300

    20-day walk forward evaluation implemeted

--- models.py
+++ models.py
@@ -19,12 +19,13 @@ def print_result(name, Y_true, Y_pred):
     f1 = f1_score(Y_true, Y_pred,average='macro')
     kappa = cohen_kappa_score(Y_true, Y_pred)
     
-    print('\nResults for', name)
+    """print('\nResults for', name)
     print('Accuracy: ', accuracy_score(Y_true, Y_pred))
     print('Precision: {:.3f} \nRecall: {:.3f} \
           \nF1 on test: {:.3f} \nCohens Kappa {:.3f}'.format(p, r, f1, kappa))
     print('Confusion Matrix: \n', confusion_matrix(Y_true, Y_pred))
-    print()
+    print()"""
+    print('\nResults for', name, p, r, f1, kappa)
     return p, r, f1, kappa
 
 def get_f1(y_true, y_pred): 
@@ -37,7 +38,7 @@ def get_f1(y_true, y_pred):
     f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
     return f1_val
 
-def benchmark(data):  
+def benchmark(data, test_lenght):  
     # Benchmark predictions for test data
     
     Y_true = np.zeros([test_lenght*N_TOP_ACTIVE, 1])
@@ -66,42 +67,23 @@ def benchmark(data):
     
     return 
 
-def split_train_test(X, Y):
+def split_train_test(X, Y, start_train, train_lenght, test_lenght):
     # Split formatted data into train and test. First train_period days for
     # training and the rest for testing
+    # Exceptions?
     
-    features = 13
-    days = X.shape[1]
+    if (start_train + train_lenght + test_lenght) > X.shape[0]/N_TOP_ACTIVE:
+        print('Check numbers')
+        return
+    N = N_TOP_ACTIVE
     
-    train_period = int(X.shape[0]/N_TOP_ACTIVE - test_lenght)
+    X_train = X[start_train*N:(train_lenght+ start_train)*N,:]  
+    Y_train = Y[start_train*N:(train_lenght+ start_train)*N,:]
     
-    X_train = np.zeros([train_period*N_TOP_ACTIVE, days, features])    
-    Y_train = np.zeros([train_period*N_TOP_ACTIVE, Y.shape[1]])
+    start_test = start_train + train_lenght
     
-    X_test = np.zeros([test_lenght*N_TOP_ACTIVE, days, features])    
-    Y_test = np.zeros([test_lenght*N_TOP_ACTIVE, Y.shape[1]])
-    
-    # Iterate through X and Y and place train_period samples for training and 
-    # next 250 for testing
-    i = 0
-    to_train_set=True
-    nth_time = 0
-    while i < X.shape[0]:        
-        if to_train_set:
-            X_train[nth_time*train_period:(nth_time+1)*train_period,:] =\
-                X[i:i+train_period,:]
-            Y_train[nth_time*train_period:(nth_time+1)*train_period,:] =\
-                Y[i:i+train_period,:]
-            i += train_period
-            to_train_set=False
-        else:
-            X_test[nth_time*test_lenght:(nth_time+1)*test_lenght,:] = \
-                X[i:i+test_lenght,:]
-            Y_test[nth_time*test_lenght:(nth_time+1)*test_lenght,:] = \
-                Y[i:i+test_lenght,:]
-            i += test_lenght
-            to_train_set=True
-            nth_time += 1
+    X_test = X[start_test*N:(test_lenght + start_test)*N,:]    
+    Y_test = Y[start_test*N:(test_lenght + start_test)*N,:]  
     
     return X_train, X_test, Y_train, Y_test
 
@@ -114,8 +96,8 @@ def format_data(data, days):
     Y = np.zeros([(data.shape[0] -days)*N_TOP_ACTIVE, 1])
     
     pos = 0
-    for i in range(N_TOP_ACTIVE):
-        for j in range(data.shape[0] - days):
+    for j in range(data.shape[0] - days):
+        for i in range(N_TOP_ACTIVE):
             # Investor i activity, eurovolume and time since last trade
             matrix = data[j:days+j,(i, i+N_TOP_ACTIVE, i+2*N_TOP_ACTIVE)] 
             
@@ -142,8 +124,7 @@ def attention_layer(inputs, length, width):
 
 def mlp_model(input_shape, neurons, net_width, net_depth, attention=False):
     
-    input_height = input_shape[1]
-    base_neurons = neurons
+    input_height = input_shape[2]
 
     inputs = Input(shape=(input_height))
     
@@ -154,7 +135,7 @@ def mlp_model(input_shape, neurons, net_width, net_depth, attention=False):
     
     for i in range(net_depth):
         
-        layer = Dense(base_neurons*net_width, activation='softmax')(layer)
+        layer = Dense(neurons*net_width, activation='softmax')(layer)
         layer = Dropout(0.5)(layer)
         
     outputs = Dense(num_classes, activation='softmax')(layer)
@@ -172,7 +153,7 @@ def train_and_testMLP(data):
     X, Y = format_data(data, 1)
     Y = to_categorical(Y, num_classes)
     
-    X_train, X_test, Y_train, Y_test = split_train_test(X, Y)
+    X_train, X_test, Y_train, Y_test = split_train_test(X, Y, 0, 664, 250)
     
     X_train = X_train[:,-1,:]
     X_test = X_test[:,-1,:]
@@ -208,8 +189,6 @@ def lstm_model(input_shape, neurons, net_width, net_depth, attention=False):
     time_steps = input_shape[1]
     features = input_shape[2]
     
-    base_neurons = neurons
-    
     inputs = Input(shape=(time_steps, features))
     
     if attention:
@@ -221,9 +200,9 @@ def lstm_model(input_shape, neurons, net_width, net_depth, attention=False):
         # return sequences True, excpet on the last layer
         return_sequences = True if not (i+1 == net_depth) else False
             
-        layer = LSTM(base_neurons*net_width, activation='softmax', 
+        layer = LSTM(neurons*net_width, activation='softmax', 
                      return_sequences=return_sequences)(layer)
-        layer = Dropout(0.5)(layer)
+        layer = Dropout(0.33)(layer)
     
     outputs = Dense(num_classes, activation='sigmoid', dynamic=True)(layer)
     
@@ -235,19 +214,19 @@ def lstm_model(input_shape, neurons, net_width, net_depth, attention=False):
 
 def train_and_testLSTM(data):   
     
-    window = 10
-    neurons = 15
+    window = 20
+    neurons = 12
             
     X, Y = format_data(data, window)
     Y = to_categorical(Y, num_classes)
     
-    X_train, X_test, Y_train, Y_test = split_train_test(X, Y)
+    X_train, X_test, Y_train, Y_test = split_train_test(X, Y, 0, 664, 250)
            
     Y_true = np.argmax(Y_test, axis=1) 
     results = list()
     
-    for net_width in [1, 2, 3]:
-        for net_depth in [1, 2]:
+    for net_width in [1, 2, 3, 4, 5]:
+        for net_depth in [1]:
                 model_lstm = lstm_model(X_train.shape, neurons, 
                                         net_width, net_depth)
     
@@ -306,7 +285,7 @@ def train_and_testCNN(data):
     X, Y = format_data(data, window)
     Y = to_categorical(Y, num_classes)
     
-    X_train, X_test, Y_train, Y_test = split_train_test(X, Y)
+    X_train, X_test, Y_train, Y_test = split_train_test(X, Y ,  0, 664, 250)
     
     # Weights?
     
@@ -336,20 +315,94 @@ def train_and_testCNN(data):
 
     return model_cnn
 
+def train_and_test_model(X, Y, model_type):
+    
+    neurons = 16 
+    results = list()
+    
+    for net_width in [3]:
+        for net_depth in [1]:
+                if model_type == 'MLP':
+                    model = mlp_model(X.shape, neurons, 
+                                          net_width, net_depth)
+                elif model_type == 'CNN':
+                    model = cnn_model(X.shape, neurons, 
+                                      net_width, net_depth)
+                elif model_type == 'LSTM':
+                    model = lstm_model(X.shape, neurons, 
+                                      net_width, net_depth)
+                name = model_type + str(net_width) + '-' + str(net_depth)
+                
+                model_results = list() 
+                train_set = 1 
+                start = 0
+                first_lenght = 654
+                normal_lenght = 20
+                test_lenght = 20
+                train_lenght = first_lenght + normal_lenght
+                while train_set <= 12:
+                
+                    X_train, X_test, Y_train, Y_test = split_train_test(
+                        X, Y, start, train_lenght, test_lenght)
+                    
+                    print('Training set ', train_set, ' from ', start, ' to ', 
+                          start + train_lenght, ' with test until', 
+                          start + train_lenght + test_lenght)
+                    
+                    if model_type == 'MLP':
+                        X_train = X_train[:,-1,:]
+                        X_test = X_test[:,-1,:]
+                        
+                        Y_true = np.argmax(Y_test, axis=1)
+                        
+                    elif model_type == 'CNN':
+                        Y_true = np.reshape(Y_test, 
+                                            (Y_test.shape[0], num_classes))
+                        Y_true = np.argmax(Y_true, axis=1)
+                    
+                
+                    model.fit(X_train, Y_train, epochs=1, verbose=1,
+                              batch_size=16, class_weight=class_weight)
+                
+                    prob = model.predict(X_test)
+                    Y_pred = np.argmax(prob, axis=1)
+                    
+                    p, r, f1, kappa = print_result(name, Y_true, Y_pred)
+                    model_results.append([p, r, f1, kappa])
+                    
+                    train_set += 1
+                    start += train_lenght 
+                    train_lenght = normal_lenght
+                
+                results.append([name, np.mean(np.asarray(model_results), 
+                                             axis=0)])
+                
+    for row in results:
+        print(row)
+    
+    return 
 
-loc = 'C:\\Users\\enehl\\OneDrive - TUNI.fi\\Opiskelu\\Kandi\\Code'  
-data = np.load(loc + '\\data.npy')
+
+
+
+loc = 'C:\\Users\\enehl\\OneDrive - TUNI.fi\\Opiskelu\\Kandi'  
+data = np.load(loc + '\\Code\\data.npy')
 
 N_TOP_ACTIVE = 100
 num_classes = 3
-test_lenght = 250
-
 
 weight = 5.
 
 class_weight = {0: 1.,
                 1: weight,
                 2: weight}
+
+window = 20
+X, Y = format_data(data, window)
+Y = to_categorical(Y, num_classes)
+
+# 20 day walk forward validation
+train_and_test_model(X, Y, 'MLP')
 
 #model_mlp = train_and_testMLP(data)
 #print(model_mlp.layers[1].get_weights())
@@ -360,7 +413,7 @@ class_weight = {0: 1.,
 #model_lstm = train_and_testLSTM(data)
 #print(model_lstm.layers[1].get_weights())
 
-#benchmark(data)
+#benchmark(data, 12*20)
 
 
 
